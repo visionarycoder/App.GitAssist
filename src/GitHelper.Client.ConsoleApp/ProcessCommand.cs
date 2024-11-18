ba69c4e0149313be293754215b0757b747cee74a
Ivan Jones <visionarycoder@users.noreply.github.com>
2024-11-17 18:09:33 -0800

    Cleaning up projects.

--- src/GitHelper.Client.ConsoleApp/ProcessCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace vc.GitHelper;
-
-public class ProcessCommand
-{
-    public string Prefix { get; init; } = string.Empty;
-    public string TargetFolder { get; init; } = string.Empty;
-    public string Suffix { get; init; } = string.Empty;
-}
